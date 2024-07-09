13c54b56a2120cf4726b12b230c5490d1e44d630
priultimus <29077634+Priultimus@users.noreply.github.com>
2024-07-09 09:13:28 -0400

    remove redundant code

--- app/resources/food.py
+++ app/resources/food.py
@@ -52,18 +52,13 @@ class ManageFood(Resource):
 
     def put(self, entity_id):
         data = request.get_json()
-        if not data.get("food"):
-            raise MissingEntryData
         food_data = data.get("food")
+        if not food_data:
+            raise MissingEntryData
 
         food_entity = core.find.food(entity_id)
         if not food_entity:
             raise EntryNotFound
-
-        food_data = data.get("food")
-
-        if not food_data:
-            raise MissingEntryData
 
         failure = []
         total_values = 0
@@ -71,29 +66,25 @@ class ManageFood(Resource):
 
         if food_data.get("name"):
             total_values += 1
-            result = core.update.food_name(entity_id, food_data.get("name"))
-            if not result:
+            if not core.update.food_name(entity_id, food_data.get("name")):
                 success = False
                 failure.append("name")
 
         if food_data.get("price"):
             total_values += 1
-            result = core.update.food_price(entity_id, food_data.get("price"))
-            if not result:
+            if not core.update.food_price(entity_id, food_data.get("price")):
                 success = False
                 failure.append("price")
 
         if food_data.get("category"):
             total_values += 1
-            result = core.update.food_category(entity_id, food_data.get("category"))
-            if not result:
+            if not core.update.food_category(entity_id, food_data.get("category")):
                 success = False
                 failure.append("category")
 
         if food_data.get("size"):
             total_values += 1
-            result = core.update.food_size(entity_id, food_data.get("size"))
-            if not result:
+            if not core.update.food_size(entity_id, food_data.get("size")):
                 success = False
                 failure.append("size")
 
