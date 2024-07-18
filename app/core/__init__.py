5475d2d6e71865b329291f97342c749f4164c6ea
Priultimus <29077634+Priultimus@users.noreply.github.com>
2024-07-18 13:28:11 -0400

    move completely to backend folder, in preperation for adding frontend

--- app/core/__init__.py
+++
@@ -1,9 +0,0 @@
-from .create import Create
-from .delete import Delete
-from .find import Find
-from .update import Update
-
-create = Create()
-delete = Delete()
-find = Find()
-update = Update()
--- app/resources/__init__.py
+++
@@ -1,3 +0,0 @@
-from .addon import CreateAddon, ManageAddon
-from .food import CreateFood, ManageFood
-from .customer import CreateCustomer, ManageCustomer
