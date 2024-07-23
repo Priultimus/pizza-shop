ab7ca78d2670481211069287343a3b735044360e
Priultimus <29077634+Priultimus@users.noreply.github.com>
2024-07-23 12:12:02 -0400

    add new helper function for cleaning data

---
+++ backend/app/resources/helper.py
@@ -0,0 +1,16 @@
+import json
+import datetime
+from typing import Union
+
+class DateTimeEncoder(json.JSONEncoder):
+    def default(self, z):
+        if isinstance(z, datetime.datetime):
+            return (str(z))
+        else:
+            return super().default(z)
+
+def clean_data(data: dict, serialize=False) -> Union[dict, str]:
+    data = json.dumps(data, cls=DateTimeEncoder)
+    if serialize:
+        return data
+    return json.loads(data)
