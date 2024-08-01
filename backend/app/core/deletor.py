9f29568d055d3368a987f3406447b355ffc789a5
Priultimus <29077634+Priultimus@users.noreply.github.com>
2024-08-01 09:51:56 -0400

    fix data inconsistencies in deletion

--- backend/app/core/deletor.py
+++ backend/app/core/deletor.py
@@ -1,10 +1,12 @@
 import logging
 from typing import Union
 
+from sqlalchemy.exc import IntegrityError
+
 from . import find
 
 from ..database import ManageResturantData, ViewResturantData
-from ..errors import EntityNotFound
+from ..errors import EntityNotFound, MustDeleteOrders
 
 
 class Delete:
@@ -37,11 +39,19 @@ class Delete:
 
         return True
 
-    def customer(self, customer_id: int) -> bool:
+    def customer(self, customer_id: int, delete_orders=True) -> bool:
+        if delete_orders:
+            orders = self.viewer.view_customer_orders(customer_id)
+            for order in orders:
+                self.order(order.order_id)
         try:
             self.manager.remove_customer(customer_id)
         except EntityNotFound:
             return False
+        except IntegrityError:
+            if not delete_orders:
+                raise MustDeleteOrders("This customer has orders that must be deleted first.")
+            raise
         except Exception as e:
             self.logger.warning("An unhandled error occurred deleting the customer.")
             self.logger.warning(f"Customer ID: {customer_id}")
@@ -49,6 +59,25 @@ class Delete:
         return True
 
     def order(self, order_id: int) -> bool:
+        # frankly, if none of these exist it doesn't really matter
+        # altho, this implementation is ugly, idk how to do better
+        try:
+            customer_id = self.viewer.view_order_customer(order_id).customer_id
+            self.manager.remove_customer_order(customer_id, order_id)
+        except EntityNotFound:
+            pass
+
+        try:
+            order_items = [x.order_item_id for x in self.viewer.view_order_items(order_id)]
+            for order_item_id in order_items:
+                try:
+                    self.manager.remove_item_mods(order_item_id)
+                except EntityNotFound:
+                    pass
+            self.manager.remove_order_items(order_id)
+        except EntityNotFound:
+            pass
+
         try:
             self.manager.remove_order(order_id)
         except EntityNotFound:
