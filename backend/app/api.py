dcc65584e0c8062dc722a42483ac19593a83b0aa
Priultimus <29077634+Priultimus@users.noreply.github.com>
2024-08-01 09:42:15 -0400

    don't pass SQLAlchemy error codes to user

--- backend/app/api.py
+++ backend/app/api.py
@@ -32,11 +32,20 @@ class ExtendedAPI(Api):
 
         # If message & code attributes are not set, consider it as Python core exception and hide sensitive error info from end user
         # Otherwise, handle application specific custom exceptions
+        
+        # Don't pass SQLALCHEMY error codes to the client
+        code = getattr(err, "code", GENERIC_SERVER_ERROR)
+        try:
+            code = int(code)
+        except ValueError:
+            logger.warning(f"Exception thrown had code {code} - replacing with GENERIC_SERVER_ERROR")
+            code = GENERIC_SERVER_ERROR
+
         response = {
             "success": getattr(err, "success", False),
             "message": getattr(err, "message", "An unexpected error occurred"),
             "data": getattr(err, "data", {}),  # additional data (if any)
-            "code": getattr(err, "code", GENERIC_SERVER_ERROR),
+            "code": code,
         }, getattr(err, "http_status_code", 500)
         if not response[0]["code"] in [
             ENTRY_NOT_FOUND,
